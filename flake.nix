4f0d506b5fdaf3fde443804ee93eb6e6a41af570
Avimitin <dev@avimit.in>
2023-06-28 16:57:05 +0800

    Fix the rust-analyzer issue

--- flake.nix
+++ flake.nix
@@ -13,13 +13,21 @@
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
+
+        toolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
       in with pkgs; {
         devShells.default = mkShell {
           buildInputs = [
             # Including cargo,clippy,cargo-fmt
-            rust-bin.stable.latest.default
+            toolchain
+            # rust-analyzer comes from nixpkgs toolchain, I want the unwrapped version
             rust-analyzer-unwrapped
           ];
+
+          # Some environment to make rust-analyzer work correctly (Still the path prefix issue)
+          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
         };
       });
 }
